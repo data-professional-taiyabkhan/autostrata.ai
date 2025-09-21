cec02586fee4ee2c658a3631ae9fa704228b6608
Mohammad Taiyab Khan <mohdtaiyabkhan19@gmail.com>
2025-09-22 00:15:00 +0100

    vercel: config

--- components/AnimatedSection.tsx
+++ components/AnimatedSection.tsx
@@ -3,14 +3,15 @@ import { motion } from 'framer-motion';
 
 export default function AnimatedSection({ children, delay=0 }:{ children: React.ReactNode, delay?: number }){
   return (
-    <motion.section
-      initial={{ opacity: 0, y: 16 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, margin: "-80px" }}
-      transition={{ duration: .7, ease: "easeOut", delay }}
-      className="section container"
-    >
-      {children}
-    </motion.section>
+    <section className="section container">
+      <motion.div
+        initial={{ opacity: 0, y: 16 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true, margin: "-80px" }}
+        transition={{ duration: .7, ease: "easeOut", delay }}
+      >
+        {children}
+      </motion.div>
+    </section>
   );
 }
